aa91e2fbcc737341097ffa1082eb83cdecd4618e
Hyuni Jung <0311jes@gmail.com>
2025-02-20 20:07:39 +0900

    SOHARD2

--- main.js
+++ main.js
@@ -120,12 +120,17 @@ function render(){
 
 function toggleComplete(id) {
     
+    
+
+
+
     for(let i =0; i < taskList.length; i++){
         if(taskList[i].id == id){
             taskList[i].isComplete = !taskList[i].isComplete;
             break;
         }
     }
+
     render();
 }
 
